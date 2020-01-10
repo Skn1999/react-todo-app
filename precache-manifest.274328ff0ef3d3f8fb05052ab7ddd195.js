3937bd5005b3ce6081b54f35291c8c5b04fb6afe
Skn1999 <sknayyar.sk@gmail.com>
2020-01-10 22:55:23 +0530

    Updates

---
+++ precache-manifest.274328ff0ef3d3f8fb05052ab7ddd195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9558689cb02b9e1bb9b609ace94b99e6",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "02a9ced61b552b6e1cc6",
+    "url": "/react-todo-app/static/css/main.0ae1cc15.chunk.css"
+  },
+  {
+    "revision": "7b1810b62d05c7d77aa9",
+    "url": "/react-todo-app/static/js/2.fe9b04ae.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo-app/static/js/2.fe9b04ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02a9ced61b552b6e1cc6",
+    "url": "/react-todo-app/static/js/main.2708ccf6.chunk.js"
+  },
+  {
+    "revision": "b43411aa2e3646c1de65",
+    "url": "/react-todo-app/static/js/runtime-main.576d6401.js"
+  }
+]);
